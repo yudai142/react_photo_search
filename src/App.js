402381c7bcd1ded9438fe9136fb2ac06c0f7e09a
yudaishimoji42865233 <yudai.shimoji.42865233@gmail.com>
2021-07-24 19:37:12 +0900

    console.log上で検索機能実装

--- src/App.js
+++ src/App.js
@@ -2,18 +2,47 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-  useEffect(() => {
-    console.log(count)
-  }, [count])
+  const [images, setImages] = useState([]);
+  const [text, setText] = useState('');
+  const [query, setQuery] = useState('apple');
+
+  useEffect(() =>{
+    console.log('useEffectが走りました')
+    fetch(`https://api.unsplash.com/search/photos?query=${query}&client_id=${process.env.REACT_APP_CLIENT_ID}`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+      setImages(data.results)
+    })
+  }, [query])
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setQuery(text);
+    setText('');
+    console.log("onSubmitが呼ばれました")
+  }
+
   return (
-    <div>
-      <p>You clicked {count} times. </p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
-      </button>
+    <div className="App">
+      <div className="main">
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            onChange={e => setText(e.target.value)}
+            value = {text}
+          />
+          <button type="submit">
+            search
+          </button>
+        </form>
+      </div>
     </div>
   )
 }
 
 export default App;
+
+// fetch('https://api.unsplash.com/search/photos?query=apple&client_id=kbEsWmKFuJvq29iEWkHoBmgFQP_RRGanAHIchGay9Qo')
+// .then(response => response.json())
+// .then(data => console.log(data))
