6b2d91ff6b9f3f87bbe25470cfe7c84ffae48bb0
yudaishimoji42865233 <yudai.shimoji.42865233@gmail.com>
2021-07-24 18:36:09 +0900

    useStateとuseEffectの確認

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    console.log(count)
+  }, [count])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>You clicked {count} times. </p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
     </div>
-  );
+  )
 }
 
 export default App;
